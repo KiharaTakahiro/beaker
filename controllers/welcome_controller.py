f356770cddfc247dfa759eea7f615ba917f316ee
KiharaTakahiro <takahirokihara123@gmail.com>
2022-01-14 17:43:05 +0900

    ログ出力の確認ロジックの追加

--- controllers/welcome_controller.py
+++ controllers/welcome_controller.py
@@ -1,7 +1,11 @@
-from common.beaker import render_template, request
+from common.beaker import render_template, request, logger
 
 def welcome():
   welcome_text = "WLECOME BEAKER"
+  logger.debug('debugログの出力')
+  logger.info('infoログの出力')
+  logger.warning('warningログの出力')
+  logger.error('errorログの出力')
   return render_template('welcome.html', welcome_text = welcome_text)
 
 def welcome_post():
