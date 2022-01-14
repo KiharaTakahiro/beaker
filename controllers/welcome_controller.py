500edc462a7bdab62f81fae60048aa4b459c5dbd
KiharaTakahiro <takahirokihara123@gmail.com>
2022-01-14 17:51:42 +0900

    セッション関連の例

--- controllers/welcome_controller.py
+++ controllers/welcome_controller.py
@@ -1,4 +1,4 @@
-from common.beaker import render_template, request, logger
+from common.beaker import get_session, set_session, render_template, request, logger
 
 def welcome():
   welcome_text = "WLECOME BEAKER"
@@ -6,7 +6,10 @@ def welcome():
   logger.info('infoログの出力')
   logger.warning('warningログの出力')
   logger.error('errorログの出力')
+  set_session('key', 'value')
   return render_template('welcome.html', welcome_text = welcome_text)
 
 def welcome_post():
+  session_value = get_session('key')
+  logger.debug(f'session_value: {session_value}')
   return render_template('welcome-post.html', post_text = request.form['welcome-text'])
